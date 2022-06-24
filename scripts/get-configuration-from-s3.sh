90713fda93c8e9a9a427309c90129f317d64e39b
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-06-25 00:11:02 +0100

    Fixed serverless configuration (#6)

--- scripts/get-configuration-from-s3.sh
+++ scripts/get-configuration-from-s3.sh
@@ -2,7 +2,7 @@
 KEY="serverless"
 
 # Full S3 path
-FROM="s3://$BUCKET/$KEY/"
+COPY_FROM="s3://$BUCKET/$KEY/"
 
 # Back to root level
 cd ..
@@ -10,4 +10,4 @@ cd ..
 # Target directory should be the one who contains the serverless.yml
 # The scripts works under the assumption we have COPY_TO defined as environment variable
 # We then copy everything under the serverless folder in S3 to the targeted directory
-aws s3 cp $FROM $TO --recursive
+aws s3 cp $COPY_FROM $COPY_TO --recursive
